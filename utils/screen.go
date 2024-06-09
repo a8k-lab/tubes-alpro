20c3f3db9e7d6cef5f4ff36aa2d72de1cb06d3d1
Abdul <malik301002@gmail.com>
2024-06-10 00:56:34 +0700

    feat(utils): windows clear screen

--- utils/screen.go
+++ utils/screen.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 )
 
+func runCmd(name string, arg ...string) {
+	cmd := exec.Command(name, arg...)
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
 func ClearScreen() {
-	c := exec.Command("clear")
-	c.Stdout = os.Stdout
-	c.Run()
+	switch runtime.GOOS {
+	case "linux":
+		runCmd("clear")
+	case "windows":
+		runCmd("cmd", "/c", "cls")
+	default:
+		runCmd("clear")
+	}
 }
 
 func ShowEmptyItemList() {
